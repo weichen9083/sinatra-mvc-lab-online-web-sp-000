bca569a079dd32fdb6fc0c1ca50054a4db90cb49
wei chen <weichen9083@gmail.com>
2021-06-15 16:23:48 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+
+get '/' do
+  erb :user_input
+end 
+
+post '/piglatinize' do 
+
+  ring = PigLatinizer.new
+  @string = ring.pig(params[:user_phrase])  
+  erb :piglatinize
+
+end 
+
+
 end
